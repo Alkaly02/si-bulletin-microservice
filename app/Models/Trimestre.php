09721bcf78bf31a7c0d298137a0eab25e39924db
Moussa Alkaly BADJI <lka.dev02@gmail.com>
2023-11-04 11:29:37 +0000

    feat:definition des relations

--- app/Models/Trimestre.php
+++ app/Models/Trimestre.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Trimestre extends Model
 {
     use HasFactory;
+
+    public function bulletins():HasMany
+    {
+        return $this->hasMany(Bulletin::class);
+    }
 }
